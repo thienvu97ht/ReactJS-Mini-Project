982265f5dc75c421cbddd7a17496d9abd3f732d6
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-04-24 16:26:07 +0700

    Project 1 b1

---
+++ setting-text-color/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import "./App.css";
+import ColorPicker from "./components/ColorPicker";
+import SizeSetting from "./components/SizeSetting";
+import Reset from "./components/Reset";
+import Result from "./components/Result";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: "red",
+      fontSize: 15,
+    };
+  }
+
+  onSetColor = (params) => {
+    this.setState({
+      color: params,
+    });
+  };
+
+  render() {
+    return (
+      <div className="container mt-50">
+        <div className="row">
+          <ColorPicker
+            color={this.state.color}
+            onReceiveColor={this.onSetColor}
+          />
+          <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <SizeSetting />
+            <Reset />
+          </div>
+          <Result color={this.state.color} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
