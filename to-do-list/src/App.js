6f876275feb1df1b2c2a96e8ae45bc7d3c70c382
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-04-26 22:04:38 +0700

    Bai 19

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -9,15 +9,16 @@ class App extends Component {
     super(props);
     this.state = {
       tasks: [], // id : unique, name, status
+      isDisplayForm: false,
     };
   }
 
   componentDidMount() {
-    if(localStorage && localStorage.getItem('tasks')) {
-      var tasks = JSON.parse(localStorage.getItem('tasks'));
+    if (localStorage && localStorage.getItem("tasks")) {
+      var tasks = JSON.parse(localStorage.getItem("tasks"));
       this.setState({
-        tasks: tasks
-      })
+        tasks: tasks,
+      });
     }
   }
 
@@ -25,41 +26,68 @@ class App extends Component {
     var tasks = [
       {
         id: this.generateID(),
-        name: 'Học lập trình ReactJS',
+        name: "Học lập trình ReactJS",
         status: true,
       },
       {
         id: this.generateID(),
-        name: 'Học lập trình Agular',
+        name: "Học lập trình Agular",
         status: false,
       },
       {
         id: this.generateID(),
-        name: 'Học lập trình Javascript',
+        name: "Học lập trình Javascript",
         status: true,
       },
     ];
 
     this.setState({
-      tasks : tasks,
+      tasks: tasks,
     });
 
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 
-    console.log(this.state.tasks)
+    console.log(this.state.tasks);
   };
 
   s4() {
-    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
   }
 
   generateID() {
-    return this.s4() + this.s4() + '-' + this.s4() + '-' + this.s4() + 
-    '_' + this.s4() + '-' + this.s4() + this.s4() + this.s4();
+    return (
+      this.s4() +
+      this.s4() +
+      "-" +
+      this.s4() +
+      "-" +
+      this.s4() +
+      "_" +
+      this.s4() +
+      "-" +
+      this.s4() +
+      this.s4() +
+      this.s4()
+    );
+  }
+
+  onToggleForm = () => {
+    this.setState({
+      isDisplayForm: !this.state.isDisplayForm
+    })
+  }
+
+  onCloseForm = () => {
+    this.setState({
+      isDisplayForm: false
+    })
   }
 
   render() {
-    var { tasks } = this.state; // var tasks = this.state.tasks; (Destructuring)
+    var { tasks, isDisplayForm } = this.state; // var tasks = this.state.tasks; (Destructuring)
+    var elmTaskForm = isDisplayForm ? <TaskForm onCloseForm={this.onCloseForm}/> : "";
     return (
       <div className="container">
         <div className="text-center">
@@ -67,12 +95,23 @@ class App extends Component {
           <hr />
         </div>
         <div className="row">
-          <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+          <div
+            className={
+              isDisplayForm ? "col-xs-4 col-sm-4 col-md-4 col-lg-4" : ""
+            }>
             {/* Form */}
-            <TaskForm />
+            {elmTaskForm}
           </div>
-          <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-            <button type="button" className="btn btn-primary">
+          <div
+            className={
+              isDisplayForm
+                ? "col-xs-8 col-sm-8 col-md-8 col-lg-8"
+                : "col-xs-12 col-sm-12 col-md-12 col-lg-12"
+            }>
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={this.onToggleForm}>
               <span className="fa fa-plus mr-5"></span>Thêm Công Việc
             </button>
             <button
@@ -86,7 +125,7 @@ class App extends Component {
             {/* List */}
             <div className="row mt-15">
               <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <TaskList tasks={tasks}/>
+                <TaskList tasks={tasks} />
               </div>
             </div>
           </div>
