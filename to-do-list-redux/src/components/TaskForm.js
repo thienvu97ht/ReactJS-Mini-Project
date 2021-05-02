d5d4508453bd305b8ae27237fd023c00de4d090b
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-05-02 14:21:58 +0700

    Bai 31

---
+++ to-do-list-redux/src/components/TaskForm.js
@@ -0,0 +1,132 @@
+import React, { Component } from "react";
+
+class TaskForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "",
+      name: "",
+      status: false,
+    };
+  }
+
+  componentDidMount = () => {
+    if (this.props.task) {
+      this.setState({
+        id: this.props.task.id,
+        name: this.props.task.name,
+        status: this.props.task.status,
+      });
+    }
+  };
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.task) {
+      if (nextProps.task.id !== prevState.id) {
+        return {
+          id: nextProps.task.id,
+          name: nextProps.task.name,
+          status: nextProps.task.status,
+        };
+      }
+    } else {
+      if (prevState.id) {
+        return {
+          id: "",
+          name: "",
+          status: true,
+        };
+      }
+    }
+    return null;
+  }
+
+  onCloseForm = () => {
+    this.props.onCloseForm();
+  };
+
+  onChange = (e) => {
+    var target = e.target;
+    var name = target.name;
+    var value = target.value;
+    if (name === "status") {
+      value = target.value === "true" ? true : false;
+    }
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+
+    // Clear vả Close form
+    this.onCloseForm();
+    this.onClear();
+  };
+
+  onClear = () => {
+    this.setState({
+      name: "",
+      status: false,
+    });
+  };
+
+  render() {
+    var { id } = this.state;
+    return (
+      <div className="panel panel-warning">
+        <div className="panel-heading">
+          <h3 className="panel-title">
+            {id !== "" ? "Cập nhật công việc" : "Thêm Công Việc"}
+            <span
+              className="fa fa-times-circle text-right"
+              onClick={this.onCloseForm}
+            />
+          </h3>
+        </div>
+        <div className="panel-body">
+          <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <label>Tên :</label>
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                value={this.state.name}
+                onChange={this.onChange}
+              />
+            </div>
+            <label>Trạng Thái :</label>
+            <select
+              className="form-control"
+              name="status"
+              value={this.state.status}
+              onChange={this.onChange}>
+              <option value={true}>Kích Hoạt</option>
+              <option value={false}>Ẩn</option>
+            </select>
+            <br />
+            <div className="text-center">
+              <button type="submit" className="btn btn-warning">
+                <span className="fa fa-plus mr-5" />
+                Lưu Lại
+              </button>
+              &nbsp;
+              <button
+                type="button"
+                className="btn btn-danger"
+                onClick={this.onClear}>
+                <span className="fa fa-close mr-5" />
+                Hủy Bỏ
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TaskForm;
