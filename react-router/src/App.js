530cd53c106f96d9a8bb063254773a24d2d14c06
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-05-20 12:39:23 +0700

    Bai 74

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,28 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
-
-const MenuLink = ({ label, to, activeOnlyWhenExact }) => {
-  return (
-    <Route
-      path={to}
-      exact={activeOnlyWhenExact}
-      children={({ match }) => {
-        var active = match ? "active abc" : "";
-        return (
-          <li className={active}>
-            <Link to={to} className="my-link">
-              {label}
-            </Link>
-          </li>
-        );
-      }}
-    />
-  );
-};
+import NotFound from "./components/NotFound";
+import Menu from "./components/Menu";
 
 class App extends Component {
   render() {
@@ -30,17 +13,14 @@ class App extends Component {
       <Router>
         <div className="App">
           {/* Menu */}
-          <nav className="navbar navbar-default">
-            <ul className="nav navbar-nav">
-                <MenuLink label="Trang Chủ" to="/" activeOnlyWhenExact={true} />
-                <MenuLink label="Giới Thiệu" to="/about" activeOnlyWhenExact={false} />
-                <MenuLink label="Liên Hệ" to="/contact" activeOnlyWhenExact={false} />
-            </ul>
-          </nav>
+          <Menu />
           {/* Nội Dung */}
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={NotFound}/>
+          </Switch>
         </div>
       </Router>
     );
