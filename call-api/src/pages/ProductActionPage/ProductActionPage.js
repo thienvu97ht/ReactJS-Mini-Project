8f64b046a3f07092f7217985092b5ce748db83e0
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-05-23 21:44:08 +0700

    Bai 94

--- call-api/src/pages/ProductActionPage/ProductActionPage.js
+++ call-api/src/pages/ProductActionPage/ProductActionPage.js
@@ -1,18 +1,64 @@
 import React, { Component } from "react";
+import callApi from "../../utils/apiCaller";
 
 class ProductActionPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "",
+      txtName: "",
+      txtPrice: "",
+      chkbStatus: "",
+    };
+  }
+
+  onChange = (e) => {
+    var target = e.target;
+    var name = target.name;
+    var value = target.type === "checkbox" ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  onSave = (e) => {
+    e.preventDefault();
+    var { txtName, txtPrice, chkbStatus } = this.state;
+
+    callApi("products", "POST", {
+      name: txtName,
+      price: txtPrice,
+      status: chkbStatus,
+    }).then(res => {
+      console.log(res);
+    });
+  };
+
   render() {
+    var { txtName, txtPrice, chkbStatus } = this.state;
     return (
       <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-        <form>
+        <form onSubmit={this.onSave}>
           <div className="form-group">
             <label>Tên Sản Phẩm:</label>
-            <input type="text" className="form-control" />
+            <input
+              type="text"
+              className="form-control"
+              name="txtName"
+              value={txtName}
+              onChange={this.onChange}
+            />
           </div>
 
           <div className="form-group">
             <label>Giá:</label>
-            <input type="number" className="form-control" />
+            <input
+              type="number"
+              className="form-control"
+              name="txtPrice"
+              value={txtPrice}
+              onChange={this.onChange}
+            />
           </div>
 
           <div className="form-group">
@@ -20,7 +66,12 @@ class ProductActionPage extends Component {
           </div>
           <div className="checkbox">
             <label>
-              <input type="checkbox" />
+              <input
+                type="checkbox"
+                name="chkbStatus"
+                value={chkbStatus}
+                onChange={this.onChange}
+              />
               Còn Hàng
             </label>
           </div>
