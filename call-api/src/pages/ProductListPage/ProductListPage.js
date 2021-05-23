3691df336bc494e02185ec73db1efcd28efa2059
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-05-23 20:58:10 +0700

    Bai 92

--- call-api/src/pages/ProductListPage/ProductListPage.js
+++ call-api/src/pages/ProductListPage/ProductListPage.js
@@ -2,10 +2,25 @@ import React, { Component } from "react";
 import ProductList from "./../../components/ProductList/ProductList";
 import ProductItem from "./../../components/ProductItem/ProductItem";
 import { connect } from "react-redux";
+import axios from "axios";
 
 class ProductListPage extends Component {
   render() {
-    var { products } = this.props;
+    // var { products } = this.props;
+    var products = [];
+
+    axios({
+      method: "get",
+      url: "https://60a77af83b1e130017176374.mockapi.io/api/products",
+      data: null,
+    })
+      .then(res => {
+        products = res.data
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
     return (
       <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <button type="button" className="btn btn-info mb-10">
