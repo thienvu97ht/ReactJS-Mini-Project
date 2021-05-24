ae6a328f5bcfdb8d3038be090470cec49a87e582
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-05-24 17:14:59 +0700

    Bai 100

--- call-api/src/index.js
+++ call-api/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import appReducers from "./reducers/index";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
-const store = createStore(
-  appReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(appReducers, composeEnhancer(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
