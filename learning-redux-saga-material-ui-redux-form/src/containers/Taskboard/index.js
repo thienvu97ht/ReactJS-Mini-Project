baa051e781dc173da77a702c55429b42073ae0c1
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-05-26 18:46:42 +0700

    Bai 114

--- learning-redux-saga-material-ui-redux-form/src/containers/Taskboard/index.js
+++ learning-redux-saga-material-ui-redux-form/src/containers/Taskboard/index.js
@@ -6,6 +6,7 @@ import AddIcon from "@material-ui/icons/Add";
 import Grid from "@material-ui/core/Grid";
 import { STATUSES } from "../../constants";
 import TaskList from "../../components/TaskList/index";
+import TaskForm from "../../components/TaskForm/index";
 
 const listTask = [
   {
@@ -29,6 +30,13 @@ const listTask = [
 ];
 
 class TaskBoard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+
   renderBoard = () => {
     let xhtml = null;
     xhtml = (
@@ -37,10 +45,31 @@ class TaskBoard extends Component {
           const taskFilter = listTask.filter(
             (task) => task.status === status.value
           );
-          return <TaskList key={status.value} task={taskFilter} status={status}/>;
+          return (
+            <TaskList key={status.value} task={taskFilter} status={status} />
+          );
         })}
       </Grid>
     );
+    return xhtml;
+  };
+
+  handleClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  openForm = () => {
+    this.setState({
+      open: true,
+    });
+  };
+
+  renderForm = () => {
+    const { open } = this.state;
+    let xhtml = null;
+    xhtml = <TaskForm open={open} onClose={this.handleClose} />;
     return xhtml;
   };
 
@@ -48,10 +77,15 @@ class TaskBoard extends Component {
     const { classes } = this.props;
     return (
       <div className={classes.taskBoard}>
-        <Button variant="contained" color="primary" className={classes.button}>
+        <Button
+          variant="contained"
+          color="primary"
+          className={classes.button}
+          onClick={this.openForm}>
           <AddIcon /> Thêm mới công việc
         </Button>
         {this.renderBoard()}
+        {this.renderForm()}
       </div>
     );
   }
