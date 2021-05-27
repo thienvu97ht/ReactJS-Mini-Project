069b8f669e3efd97e3ea641daac708635d5436ed
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-05-27 21:51:46 +0700

    Bai 129

--- learning-redux-saga-material-ui-redux-form/src/index.js
+++ learning-redux-saga-material-ui-redux-form/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./containers/App/index";
 import reportWebVitals from "./reportWebVitals";
-import './generator'
 
 ReactDOM.render(<App />, document.getElementById("root"));
 reportWebVitals();
